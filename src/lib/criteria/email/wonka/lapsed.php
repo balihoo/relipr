81dd11363116f71b7b10ff0100af9e58c1e95fec
Paul Price <pauldprice@gmail.com>
2013-06-17 08:33:52 -0600

    Added email versions of both wonka criteria

---
+++ src/lib/criteria/email/wonka/lapsed.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'directmail/wonka/lapsed.php';
+
+class email_wonka_lapsed extends directmail_wonka_lapsed { }
+
---
+++ src/lib/criteria/email/wonka/newmovers.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'directmail/wonka/newmovers.php';
+
+class email_wonka_newmovers extends directmail_wonka_newmovers { } 
+
